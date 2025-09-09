2b7a18c2c552ca6e1ee673d1feef0c5af16dd8c4
Sally <causeysally@gmail.com>
2025-09-08 19:29:27 -0500

    buttons

---
+++ script.js
@@ -0,0 +1,16 @@
+onEvent("button1", "click", function(){
+    var userName=getText("box1");
+    var icecream=getText("box2");
+  console.log(userName + "\n" + icecream);
+    var cookie=getText("box3");
+    var color=getText("box4");
+  console.log(cookie + "\n" + color);
+    var sport=getText("box5");
+    var userMessage= "Hello, " + userName + "! Wow! " + icecream + " is a great flavor! " + cookie + " is amazing cookie. " + color + " really suites you. " + sport + " really keeps you acive. ";
+  
+  setText("textbox",userMessage);
+});
+onEvent("button1", "click", function( ) {
+	console.log("button2 clicked!");
+	setScreen("page2.html");
+});jjjjjkju8kjuyinjmkjbnkuyjmhun khob8li7u9kim hyyyyyyyyyyyy /* aa.. */
